7ef7cb7115780a8014ba5a0b23f5bc4061180315
Pure Dev <pure.developer@puredev.run>
2025-03-16 15:01:14 -0400

    add direct link

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "./providers/theme-provider";
 import { PureEditor } from "./components/pure-editor";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/sonner";
 
 function App() {
   return (
     <ThemeProvider>
       <TooltipProvider>
+        <Toaster />
         <div className="h-screen">
           <PureEditor />
         </div>
--- src/components/pure-editor/index.tsx
+++ src/components/pure-editor/index.tsx
@@ -1,14 +1,19 @@
 import { LogPanel } from "@/components/pages/editor/panels/log-panel";
+import { Button } from "@/components/ui/button";
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import { VSCodeEditor } from "@/components/vscode-editor/vscode-editor";
+import { ExternalLinkIcon } from "@radix-ui/react-icons";
+import { useEffect } from "react";
+import { toast } from "sonner";
 
 export function PureEditor() {
   return (
-    <div className="h-full">
+    <div className="h-full flex flex-col">
+      <SiteHeader />
       <ResizablePanelGroup direction="vertical" className="h-full">
         <ResizablePanel defaultSize={60}>
           <VSCodeEditor file="test.ts" />
@@ -19,5 +24,68 @@ export function PureEditor() {
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
+  );
+}
+
+function SiteHeader() {
+  useEffect(() => {
+    const timer = setTimeout(
+      () => {
+        toast(
+          <div className="flex flex-col gap-2">
+            <p className="text-sm">
+              Enjoying the playground? Try Pure Dev for the same interactive
+              experience plus API creation, real-time logs, and built-in API
+              testing, and so much more!
+            </p>
+            <Button
+              variant="brand"
+              size="sm"
+              className="w-fit flex gap-2"
+              // @ts-ignore
+              href="https://puredev.run"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Try Pure Dev
+              <ExternalLinkIcon className="h-4 w-4" />
+            </Button>
+          </div>,
+          {
+            duration: 10000,
+            position: "bottom-right",
+          }
+        );
+      },
+      2 * 60 * 1000
+    ); // 2 minutes
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <header className="border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 items-center justify-between">
+        <div className="flex flex-col gap-0.5">
+          <span className="font-semibold">TypeScript Playground</span>
+          <span className="text-muted-foreground text-sm">
+            A simple playground for TypeScript
+          </span>
+        </div>
+        <Button
+          variant="brand"
+          size="xs"
+          tag={"a"}
+          // @ts-ignore
+          href="https://puredev.run"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center gap-2"
+        >
+          <a>Try Pure Dev</a>
+          <ExternalLinkIcon className="h-4 w-4" />
+        </Button>
+      </div>
+    </header>
   );
 }
